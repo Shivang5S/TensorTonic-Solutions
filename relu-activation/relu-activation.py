92153bcdaca24a806a0ffeec43e642773cf7af0b
Shivang <555shivae@gmail.com>
2026-03-10 06:43:29 +0530

    Update solution for relu-activation

--- relu-activation/relu-activation.py
+++ relu-activation/relu-activation.py
@@ -4,9 +4,13 @@ def relu(x):
     """
     Implement ReLU activation function.
     """
-    # Creating an empty np array
+    '''
+    Requirements are:
+        Return result using NumPy (not Python lists)
+        Handle scalar, list, and NumPy array inputs
+        Fully vectorized (no explicit Python loops)
+        Preserve input shape
+    '''
     x = np.asarray(x)       # handles x as scalar, list, vector, matrix
     
-    res = np.maximum(0, x)  # result array, with relu applied on x
-
-    return res
+    return np.where(x <= 0, 0, x)

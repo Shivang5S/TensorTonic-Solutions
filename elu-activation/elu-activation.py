fef85637b0372e0672691ad743bf30adf99c518c
Shivang <555shivae@gmail.com>
2026-03-10 06:39:24 +0530

    Update solution for elu-activation

--- elu-activation/elu-activation.py
+++ elu-activation/elu-activation.py
@@ -5,6 +5,7 @@ def elu(x, alpha):
     """
     # Write code here
     res = []
+    # Since, the question demands to return a list, and the input itself is a list. Also, x is not a vector (np array), we may simply use ternary operator element wise. Because np.where is useful for eliminating the loops in np arrays
     for x0 in x:
         res.append(np.where(x0 > 0, x0, alpha * (np.exp(x0) - 1)))
     

6970e0fcfbb2596f57424da7100a802d69c726ce
Shivang <555shivae@gmail.com>
2026-03-11 05:59:17 +0530

    Add solution for covariance-matrix

---
+++ covariance-matrix/covariance-matrix.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def covariance_matrix(X):
+    """
+    Compute covariance matrix from dataset X.
+    """
+    # Write code here
+    X = np.asarray(X)
+    if X.ndim != 2:
+        return None
+        
+    N = len(X)
+    if N < 2: 
+        return None
+        
+    u = np.mean(X, axis = 0)
+    X = X - u
+    # That, above is broadcasting, np aligns shapes from the right
+
+    return (X.T @ X)/(N - 1)
+    

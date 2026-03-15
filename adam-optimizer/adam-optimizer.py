6c03238b5536b7a36cb53253b44cf2343db6dece
Shivang <555shivae@gmail.com>
2026-03-15 15:29:55 +0530

    Update solution for adam-optimizer

--- adam-optimizer/adam-optimizer.py
+++ adam-optimizer/adam-optimizer.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 def adam_step(param, grad, m, v, t, lr=1e-3, beta1=0.9, beta2=0.999, eps=1e-8):
-    param, grad, m, v = np.asarray(param, dtype=float), np.asarray(grad, dtype=float), \
-                        np.asarray(m, dtype=float),     np.asarray(v, dtype=float)
+    param, grad, m, v = np.asarray(param, dtype=float), np.asarray(grad, dtype=float), np.asarray(m, dtype=float),     np.asarray(v, dtype=float)
 
     m_new = beta1 * m + (1 - beta1) * grad
     v_new = beta2 * v + (1 - beta2) * grad**2
